902b8bb3ac90633110f2c0fb326ce3e0ea5baf76
rubytorres <ruby.torres.346@my.csun.edu>
2025-12-09 21:14:03 -0800

    Finishing S1-S9

--- ListStackDriver.java
+++ ListStackDriver.java
@@ -3,6 +3,8 @@
 // DO NOT change the class name or the signature of main().
 // Implement ONLY the method for your assigned task (e.g., runS4_ReverseWord).
 
+import java.util.Scanner;
+
 public class ListStackDriver {
 
     public static void main(String[] args) {
@@ -11,12 +13,12 @@ public class ListStackDriver {
 
         runS1_BrowserBackButton();
         runS2_TextEditorUndo();
-        //runS3_BalancedParentheses();
+        runS3_BalancedParentheses();
         runS4_ReverseWord();
-        //runS5_DirectoryNavigation();
+        runS5_DirectoryNavigation();
         runS6_CalculatorHistory();
         runS7_PalindromeChecker();
-        //runS8_FunctionCallStack();
+        runS8_FunctionCallStack();
         //runS9_StackOfPlates();
     }
 
@@ -26,7 +28,7 @@ public class ListStackDriver {
         System.out.println("----------S1----------");
         ListStack<String> pageURLs = new ListStack<>();
 
-        //Pushing pages i have visted into pageURLs
+        //Push visited pages into pageURLs
         System.out.println("Page URL History: ");
         pageURLs.push("https://www.google.com");
         System.out.println(pageURLs.peek());
@@ -37,12 +39,12 @@ public class ListStackDriver {
         pageURLs.push("https://www.google.com/maps");
         System.out.println(pageURLs.peek());
 
-        //Showing the changes done to the list when you press the "Back" button
+        //Show that list changes when you press the "Back" button
         System.out.println("Current page: " + pageURLs.peek());
-        System.out.println("Pressed \"Back\" so we must pop: " + pageURLs.peek());
+        System.out.println("Pressed \"Back\"");
         pageURLs.pop();
         System.out.println("Current page is now: " + pageURLs.peek());
-        System.out.println("Pressed \"Back\" again so we must pop: " + pageURLs.peek());
+        System.out.println("Pressed \"Back\"");
         pageURLs.pop();
         System.out.println("Current page is now: " + pageURLs.peek());
     }
@@ -55,33 +57,54 @@ public class ListStackDriver {
 
         //Pushing all new words into the stack list
         System.out.print("Words in document: ");
-        textDocument.push("Hello Ruby!");
+        textDocument.push("My");
         System.out.print(textDocument.peek() + " ");
-        textDocument.push("How are you?");
+        textDocument.push("name");
         System.out.print(textDocument.peek() + " ");
-        textDocument.push("Are you okay?");
+        textDocument.push("is");
+        System.out.print(textDocument.peek() + " ");
+        textDocument.push("Ruby");
         System.out.println(textDocument.peek());
 
-        //Popping to undo a change done to the text document
-        System.out.println("Popping \"" + textDocument.peek() + "\" to undo a change");
+        //Popping to undo a change
         textDocument.pop();
+        System.out.println("Popped to undo a change\nLast word in doc is: " + textDocument.peek());
     }
 
     // S3 – Checking Balanced Parentheses
     private static void runS3_BalancedParentheses() {
         // TODO: Implement task S3 here.
         System.out.println("----------S3----------");
-        ListStack<Character> balancedParentheses = new ListStack<>();
-        balancedParentheses.push('a');
-        balancedParentheses.push('*');
-        balancedParentheses.push('(');
-        balancedParentheses.push('b');
-        balancedParentheses.push('+');
-        balancedParentheses.push('c');
-        balancedParentheses.push(')');
+        ListStack<Character> parentheses = new ListStack<>();
+        String test = "(5*8)+3)";
+        System.out.println(test);
+        boolean balanced = true;
+        String isBalanced = "";
 
+        for(char c: test.toCharArray()) {
+            if(c == '(') {
+                parentheses.push(c);
+            }
+            else if( c == ')') {
+                if(parentheses.isEmpty()) { //Checks if there is a pair for ')'
+                    balanced = false; //If stack is empty, there is no pair for ')'
+                    break;
+                }
+                parentheses.pop();
+            }
+        }
 
-        System.out.println(balancedParentheses);
+        if(!parentheses.isEmpty()){   //If stack is not empty, there are parenthesis(s) left in stack without a pair
+            balanced = false; //Balance is false because there are still parentheses(s) in stack
+        }
+        //Adding a loop to handle the message
+        if(balanced){
+            isBalanced = "Balanced";
+        }
+        else{
+            isBalanced = "Not Balanced";
+        }
+        System.out.println("Balanced expression? -> " + isBalanced);
     }
 
     // S4 – Reversing a Word Using a Stack
@@ -90,7 +113,7 @@ public class ListStackDriver {
         System.out.println("----------S4----------");
         ListStack<Character> wordChars = new ListStack<>();
 
-        //Pushing all characters of the word "magic" individually
+        //Pushing all characters of "magic" into stack
         wordChars.push('m');
         wordChars.push('a');
         wordChars.push('g');
@@ -99,7 +122,7 @@ public class ListStackDriver {
 
         System.out.print("The reversed word of magic is: ");
 
-        //Popping all characters in the list to get a reversed word
+        //Popping all characters to get a reversed word
         while(!wordChars.isEmpty()) {
             Character letter = wordChars.pop();
             System.out.print(letter);
@@ -113,7 +136,23 @@ public class ListStackDriver {
         // TODO: Implement task S5 here.
         ListStack<String> folderNames = new ListStack<>();
 
+        //Test commands
+       String[] commands = {"cd User", "cd OneDrive", "cd ..", "cd Downloads", "cd .."};
 
+       //Looping through all test commands
+       for(String command: commands) {
+           System.out.println(command);
+           if(command.equals("cd ..")){
+               if(!folderNames.isEmpty()){ //Pop() as long as there are still folder names in stack
+                   folderNames.pop();
+                   System.out.println("Current folder is: " + folderNames.peek());
+               }
+           }
+           else if(command.startsWith("cd ")){ //Push if command format is "cd folder"
+               folderNames.push(command.substring(3)); //Get the folder name by using the substring function and add it to stack
+               System.out.println("Current folder is: " + folderNames.peek());
+           }
+       }
     }
 
     // S6 – History of Calculator Operations
@@ -136,8 +175,8 @@ public class ListStackDriver {
         System.out.println(calculatorHistory.peek());
 
         //Popping to undo the last operation
-        System.out.println("Popping \"" + calculatorHistory.peek() + "\" to undo the last operation");
         calculatorHistory.pop();
+        System.out.println("Popped last operation\nLast thing in calculator: " + calculatorHistory.peek());
     }
 
     // S7 – Palindrome Checker
@@ -170,7 +209,19 @@ public class ListStackDriver {
         System.out.println("----------S8-----------");
         // TODO: Implement task S8 here.
         ListStack<String> functionNames = new ListStack<>();
+        String[] tests = {"call stack", "call main", "return", "call PalindromeChecker", "return"};
 
+        for(String test: tests) {
+            System.out.println(test);
+            if(test.startsWith("call ")) {
+                functionNames.push(test.substring(5));
+
+            }
+            else if(test.equals("return")) {
+                functionNames.pop();
+            }
+            System.out.println("Function at the top of stack: " + functionNames.peek());
+        }
     }
 
     // S9 – Stack of Plates (Capacity-Limited Stack)
@@ -179,9 +230,7 @@ public class ListStackDriver {
         // TODO: Implement task S9 here.
         ListStack<Integer> plates = new ListStack<>();
         int capacty = 5;
-        if(plates.size() < capacty) {
-
-        }
+        Scanner scanner = new Scanner(System.in);
 
 
 
